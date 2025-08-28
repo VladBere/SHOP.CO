55d1049f5508129152ebf4e3263e96c8f2b3707c
VladBere <vladbereza@ukr.net>
2025-08-29 01:43:28 +0300

    fixed window bug when card clicked

--- src/components/product/product-card.tsx
+++ src/components/product/product-card.tsx
@@ -8,11 +8,16 @@ interface ProductCardProps {
 }
 
 export const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
-    <div onClick={() => {navigate("/product/" + product.id)}} className="h-[450px] w-[295px] cursor-pointer hover:bg-gray-300/10 rounded-2xl">
+    <div
+      onClick={() => {
+        navigate("/product/" + product.id);
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      }}
+      className="h-[450px] w-[295px] cursor-pointer hover:bg-gray-300/10 rounded-2xl"
+    >
       <div className="h-[298px] w-[295px] rounded-xl overflow-hidden bg-secondaryBg mb-4 flex items-center shadow-sm shadow-black/20">
         <img src={product.images[0]} alt="" />
       </div>
@@ -21,7 +26,7 @@ export const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
         <Stars rating={product.rating} />
         <p className=" ml-[13px]">{product.rating.toFixed(1)}/5</p>
       </div>
-      <Price mode="card" product={product}/>
+      <Price mode="card" product={product} />
     </div>
   );
 };
